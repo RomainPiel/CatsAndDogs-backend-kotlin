a44c33117aec41203ea063d38715e16b9dd6c4ee
Romain Piel <romain.piel@gmail.com>
2017-02-19 17:02:00 +0000

    added static json resource

--- src/main/kotlin/com/romainpiel/catsanddogs/Main.kt
+++ src/main/kotlin/com/romainpiel/catsanddogs/Main.kt
@@ -1,11 +1,11 @@
 package com.romainpiel.catsanddogs
 
-import spark.Spark.get
-import spark.Spark.port
+import spark.Spark.*
 import java.lang.System.getenv
 
 
 fun main(args: Array<String>) {
     getenv("PORT")?.let { port(it.toInt()) }
-    get("/") { req, res -> "Cats And Dogs - Kotlin - Server Says Hello" }
+    staticFileLocation("/public")
+    get("/") { req, res ->  "Cats And Dogs - Kotlin - Server Says Hello" }
 }
